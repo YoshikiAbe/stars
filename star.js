b0911ffcff33e18e9a67c5821eb2025b33ab995f
YoshikiAbe <yoshiki.abe@jrits.ricoh.co.jp>
2016-08-08 15:55:01 +0900

    階段登り降り。真ん中２行だけ^にする

--- star.js
+++ star.js
@@ -1,25 +1,19 @@
-// class Numbers {
-//   constructer(maxNum) {
-//     this.nums: [],
-//     this.maxNum: maxNum,
-//     this.setNums: () => {
-//   }
-// };
-
 const getSequence = (length) => {
-  const sequence = Array.from({length: length}, (value, index) => index + 1);
-  return sequence;
+    const sequence = Array.from({length: length}, (value, index) => index + 1);
+    return sequence;
 };
 
 const getMark = (value) => {
-  if(value % 5 == 0) {
-    return '^';
-  }
-  return '*';
+    if(value % 5 == 0) {
+        return '^';
+    }
+    return '*';
 }
 
-getSequence(100).forEach((value, index) => {
-  const line = (index + 1) + ':';
-  const plots = getMark(value).repeat(value);
-  console.log(line + '\t' + plots);
+const upSequence = getSequence(5)
+const downSequece = getSequence(5).reverse();
+upSequence.concat(downSequece).forEach((value, index) => {
+    const line = (index + 1) + ':';
+    const plots = getMark(value).repeat(value);
+    console.log(line + '\t' + plots);
 });
