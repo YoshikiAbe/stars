ff67d745dc8cd4346e30b262f6d523bba20d9b3b
YoshikiAbe <yoshiki.abe@jrits.ricoh.co.jp>
2016-08-08 16:56:46 +0900

    関数を分割化

--- star.js
+++ star.js
@@ -1,21 +1,26 @@
-// ブラウザで実行しやすいようにletにした
-let getSequence = (length) => {
+//
+// const getMark = (value) => {
+//     if(value % 5 == 0) {
+//         return '^';
+//     }
+//     return '*';
+// }
+
+const getSequence = (length) => {
     const sequence = Array.from({length: length}, (value, index) => index + 1);
     return sequence;
 };
 
-const getMark = (value) => {
-    if(value % 5 == 0) {
-        return '^';
-    }
-    return '*';
+const printEvenLines = (args) => {
+    args.forEach((value, index) => {
+        const line = index + 1;
+        if(line % 2 !== 0) return;
+        console.log(line + ':\t' + value);
+    });
 }
 
-const sequence = getSequence(5);
-const sequenceReversed = sequence.slice().reverse();
-
-sequence.concat(sequenceReversed).forEach((value, index) => {
-    const line = (index + 1) + ':';
-    const plots = getMark(value).repeat(value);
-    console.log(line + '\t' + plots);
+const sequence = getSequence(10);
+const plots = sequence.map((value, index) => {
+    return '*'.repeat(value);
 });
+printEvenLines(plots);
